180b92ba2d69934d78da8061d3e348b37ff774aa
daeunkim701 <lionboot2025@gmail.com>
2025-08-27 11:03:46 +0900

    수정 후 실행 오나료

--- src/main/java/org/example/MemoryRepo.java
+++ src/main/java/org/example/MemoryRepo.java
@@ -1,9 +1,9 @@
 package org.example;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Component
+// @Component
+@Repository
 public class MemoryRepo {
     public void save(String data) {
         System.out.println("[MemoryRepo] 데이터 저장 %s".formatted(data));
--- src/main/java/org/example/SpringCore.java
+++ src/main/java/org/example/SpringCore.java
@@ -6,14 +6,15 @@ public class SpringCore {
     public static void main(String[] args) {
         System.out.println("----- 스프링 컨테이너 초기화 시작 -----");
 
-        // context ? -> 앱 설정 AppConfig? SpringConfig?
-        // Bean ...
-        try(AnnotationConfigApplicationContext context = new
-                AnnotationConfigApplicationContext(AppConfig.class)) {
+        // context ? -> 앱 설정. AppConfig? SpringConfig?
+        // Bean...
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
             System.out.println("----- 스프링 컨테이너 초기화 완료 -----");
             System.out.println();
+            // Spring Container
+            OrderService orderService = context.getBean(OrderService.class);
+
+            orderService.processOrder("치킨");
         }
-
-
     }
 }
